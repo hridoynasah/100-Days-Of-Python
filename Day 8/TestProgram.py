5d1886414f7ec11b8b70afd9f01bc5eeea2febdb
Hridoy <hridoynasah@gmail.com>
2024-08-07 22:21:12 +0600

    First Commit

--- Day 8/TestProgram.py
+++ Day 8/TestProgram.py
@@ -1,18 +1,23 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-text = "XYZ".lower()
-alphaList = []
-encrypted_text = ""
-shift = 3
-for i in range(len(text)):
-    for j in range(26):
-        if alphabet[j] == text[i]:
-            idx = j + shift
 
-            if idx <= 25:
-                encrypted_text += alphabet[idx]
-            else:
-                idx -= 26
-                encrypted_text += alphabet[idx]
+def CeaserCipher(text, shift):
+    end_text = ""
+    for el in text:
+        position = alphabet.index(el)
+        new_position = shift + position
+        original_shift = new_position % 26
+        end_text += alphabet[original_shift]
 
-print(f"Your encrypted message: {encrypted_text}")
+    print(end_text)
+
+def decryption(cipher_text, shift):
+    end_text =""
+    for el in cipher_text:
+        position = alphabet.index(el) - shift
+        new_position = position % 26
+        end_text += alphabet[new_position]
+    print(end_text)
+
+CeaserCipher("Hridoy".lower(), 45)
+decryption("akbwhr", 45)
 
