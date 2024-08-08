2f913561176dff52153d0712d05fbf85d0e810df
Hridoy <hridoynasah@gmail.com>
2024-08-08 16:32:14 +0600

    First Commit

---
+++ Day 9/Exercise2.py
@@ -0,0 +1,10 @@
+my_dictionary = {}
+my_dictionary["Hridoy"] = 1826
+my_dictionary["Sattik"] = 1827
+my_dictionary["Rakib"] = 1828
+my_dictionary["Hasan"] = 1829
+for key in my_dictionary:
+    print(key)
+
+for key in my_dictionary:
+    print(my_dictionary[key])
